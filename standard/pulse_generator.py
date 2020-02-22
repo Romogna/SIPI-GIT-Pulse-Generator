de71e32c8ba2e12391b842180e05ea7c2d1f67d6
Romogna <alderon13@gmail.com>
2020-02-22 03:17:28 -0700

    IP socket added to script

--- standard/pulse_generator.py
+++ standard/pulse_generator.py
@@ -1,80 +1,108 @@
 #!/usr/bin/env python
-
+import socket
 import time
-import bluetooth
 import pigpio
 
 GPIO=4 # pin selected for pwg
-square = []  # list to store the waveform parameters
 exposure_time = [] # stores a list of exposure times
-wave_param = [] # holds data from bluetooth
+wave_param = [] # holds data from the message
+square = []  # list to store the waveform parameters
 
-hostMACAddress = ''
-port = 1
-size = 1024
+def wave_creation(data):
 
-sig = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-sig.bind((hostMACAddress, port))
-sig.listen(1)
+  wave_param = data.split(',')
 
-try:
-  print("Trying")
-  client_sock,address = s.accept()
-  print("Accepted Connection from ", address)
+  #image_count = input("How many exposures will you be taking? \n")
+  image_count = int(wave_param[0])
+  #intial_exposure = input("Enter in the intial exposure time: ")
+  initial_exposure = int(wave_param[1])
+  #interval_param = input("Enter in the interval between exposures: ")
+  interval_param = int(wave_param[2])
+  #sequence_param = input ("Will the sequence of exposures be (geometric or arithmetic)? \n")
+  sequence_param = int(wave_param[3]) # 0 is geometric, 1 is arithmetic
+  #sequence_steps = input ("What is the common difference/ratio? \n")
+  sequence_steps = int(wave_param[4])
 
-  while 1:
-    data = client_sock.recv(size)
-    if data:
-      print("Received: ", data)
+  # initializing
+  exposure_time.clear()
+  square.clear()
+  compile = 1
 
-  # work in this area for bluetooth parse
-    
-    wave_creation(wave_param)
+  # loop to populate the exposure time list
+  while (compile < image_count):
+    if sequence_param == 0: # will add sequence exposures to list arithmetic
+      exposure_time.append(initial_exposure + (sequence_steps*compile))
+    else if sequence_param == 1:  # will add sequence exposures to list geometric
+      exposure_time.append(initial_exposure * (sequence_steps**compile))
 
-except:
-  print("Closing Socket")
-  client_sock.close()
-  s.close()
+    compile = compile + 1 # increment until we have enough exposures
 
+  for i in irange(0, image_count): # populates the square wave with parameters
+  # pulses                     ON       OFF      MICROS
+    square.append(pigpio.pulse(1<<GPIO, 0,       exposure_time[i])) # varying exposures
+    square.append(pigpio.pusle(0,       1<<GPIO, interval_param)) # interval between exposures
 
-def wave_creation(wave_param):
+  pi = pigpio.pi() # connect to local Pi
+  pi.set_mode(GPIO, pigpio.OUTPUT) # turns GPIO pin to output
+  pi.wave_add_generic(square) # adds a list(square) of pulses to current waveform
+  wid = pi.wave_create()  # creates a waveform with the data
 
-#image_count = input("How many exposures will you be taking? \n")
-image_count = wave_param[1]
-#intial_exposure = input("Enter in the intial exposure time: ")
-initial_exposure = wave_param[2]
-#interval_param = input("Enter in the interval between exposures: ")
-interval_param = wave_param[3]
-#sequence_param = input ("Will the sequence of exposures be (geometric or arithmetic)? \n")
-sequence_param = wave_param[4] # 0 is geometric, 1 is arithmetic
-#sequence_steps = input ("What is the common difference/ratio? \n")
-sequence_param = wave_param[5]
+  if wid >= 0:
+     pi.wave_send_once(wid) # generates the wave pulse
+     print('Producing wave now!')
+     print(square)
 
-compile = 1
-# loop to populate the exposure time list
-while (compile < image_count):
-  if sequence_param == 0: # will add sequence exposures to list arithmetic
-    exposure_time.append(initial_exposure + (sequence_steps*compile))
-  else if sequence_param == 1:  # will add sequence exposures to list geometric
-    exposure_time.append(initial_exposure * (sequence_steps**compile))
+     time.sleep(10)          # used to pause time before stopping
+     pi.wave_tx_stop()        # used to stop the pulse
+     pi.wave_delete(wid)       # used to delete the current waveform
 
-  compile = compile + 1 # increment until we have enough exposures
+  pi.stop()  # stops the local pi connection
 
-for i in irange(0, image_count): # populates the square wave with parameters
-# pulses                     ON       OFF      MICROS
-  square.append(pigpio.pulse(1<<GPIO, 0,       exposure_time[i])) # varying exposures
-  square.append(pigpio.pusle(0,       1<<GPIO, interval_param)) # interval between exposures
+###----- End of wave_creation()---------
 
-pi = pigpio.pi() # connect to local Pi
-pi.set_mode(GPIO, pigpio.OUTPUT) # turns GPIO pin to output
-pi.wave_add_generic(square) # adds a list(square) of pulses to current waveform
-wid = pi.wave_create()  # creates a waveform with the data
+if __name__ == "__main__":
 
-if wid >= 0:
-   pi.wave_send_once(wid) # generates the wave pulse
-   print('Producing wave now!')
-   time.sleep(10)          # used to pause time before stopping
-   pi.wave_tx_stop()        # used to stop the pulse
-   pi.wave_delete(wid)       # used to delete the current waveform
+  # next create a socket object
+  s = socket.socket()
+  print "Socket successfully created"
 
-pi.stop()  # stops the local pi connection
+  # reserve a port on your computer in our
+  # case it is 12345 but it can be anything
+  port = 12345
+
+  # Next bind to the port
+  # we have not typed any ip in the ip field
+  # instead we have inputted an empty string
+  # this makes the server listen to requests
+  # coming from other computers on the network
+  s.bind(('', port))
+  print "socket binded to %s" %(port)
+
+  # put the socket into listening mode
+  s.listen(5)
+  print "socket is listening"
+
+  # a forever loop until we interrupt it or
+  # an error occurs
+  while True:
+
+    # Establish connection with client.
+    c, addr = s.accept()
+    print 'Got connection from', addr
+
+    # send a thank you message to the client.
+    c.send('Thank you for connecting')
+
+    # Notes: Will do the task after receiving message
+    data = c.recv(1024)
+    print('Receiving: {}, {}'.format(data,type(data)))
+
+    wave_param.clear() # clearing the list
+    wave_generation(data)
+
+    # Close the connection with the client
+    c.close()
+
+###------End of main()----------
+
+# Message in format 1,2,3,4,5
