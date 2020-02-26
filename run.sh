1a1f34e123a0833761a4d5af7e01bac28c052db7
Romogna <alderon13@gmail.com>
2020-02-26 10:48:09 -0700

    Adjusted for final product

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@ echo "Starting PIGPIO Daemon"
 sudo pigpiod
 
 echo "Starting Pulse Width Generator"
-python3 standard/gpio_test.py
+#python3 standard/gpio_test.py
+python3 standard/pulse_generator.py
 
 echo "Closing PIGPIO Daemon"
 sudo killall pigpiod
